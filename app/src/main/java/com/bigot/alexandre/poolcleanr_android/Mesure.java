a771936722fb6e57981301abf6a53be0c2dfedca
bigotalex <alexandre.bigot9@gmail.com>
2016-11-23 22:54:19 +0100

    Page Mesures
    Get valeurs des ouverture de l'appli + informations + debut design

--- app/src/main/java/com/bigot/alexandre/poolcleanr_android/Mesure.java
+++ app/src/main/java/com/bigot/alexandre/poolcleanr_android/Mesure.java
@@ -3,11 +3,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +18,10 @@ import org.json.JSONException;
 import java.util.concurrent.ExecutionException;
 
 public class Mesure extends Fragment {
+
+    public final CharSequence toast_info_ph = "Le pH mesure l'acidité d'un liquide. Il est compris entre 0 et 14. L'eau d'une piscine a idéalement un pH compris entre 7 et 7,5.";
+    public final CharSequence toast_info_chlore = "Le taux de chlore d'une piscine doit être compris entre 40% et 60%.";
+    public final CharSequence toast_info_temp = "Vous êtes libres de déterminer la température optimale de votre piscine.";
 
     @Nullable
     @Override
@@ -24,71 +31,130 @@ public class Mesure extends Fragment {
         return inflater.inflate(R.layout.fragment_measures, container, false);
     }
 
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Mesures Piscine");
 
-        final TextView ph_mesure_output =(TextView) getView().findViewById(R.id.ph_measure_output);
-        final TextView ph_date_output = (TextView) getView().findViewById(R.id.ph_date_output);
-        Button pH_button=(Button) getView().findViewById(R.id.ph_button);
-        pH_button.setOnClickListener(new View.OnClickListener() {
+        // Get the value for each unity
+        getMesure();
+
+        // ask for a measure/get
+        Button mesure_btn =(Button) getView().findViewById(R.id.mesure_btn);
+        mesure_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Test_connectivity task = new Test_connectivity();
-                task.execute("http://loicberthelot.freeboxos.fr/device/pH/");
-                try {
-                    //get when the request is finished
-                    String response = task.get();
-                    Log.i("Content",response);
-
-                    JSONArray json = new JSONArray(response);
-                    String mesure = json.getJSONObject(0).getString("mesure");
-                    String date =json.getJSONObject(0).getString("time_of_mesure");
-
-                    ph_mesure_output.setText(mesure.toString());
-                    ph_date_output.setText(date.toString());
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                //Test_connectivity tast_order = new Test_connectivity();
+                //tast_order.execute("http://loicberthelot.freeboxos.fr/order/");
             }
         });
 
-        final TextView chlore_mesure_output =(TextView) getView().findViewById(R.id.chlore_measure_output);
-        final TextView chlore_date_output = (TextView) getView().findViewById(R.id.chlore_date_output);
-        Button chlore_button=(Button) getView().findViewById(R.id.chlore_button);
-        chlore_button.setOnClickListener(new View.OnClickListener() {
+        // Toast of information for pH
+        ImageView info_ph = (ImageView) getView().findViewById(R.id.icon_info_ph);
+        info_ph.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Test_connectivity task = new Test_connectivity();
-                task.execute("http://loicberthelot.freeboxos.fr/device/Chlore/");
-                try {
-                    //get when the request is finished
-                    String response = task.get();
-                    Log.i("Content",response);
-
-                    JSONArray json = new JSONArray(response);
-                    String mesure = json.getJSONObject(0).getString("mesure");
-                    String date =json.getJSONObject(0).getString("time_of_mesure");
-
-                    chlore_mesure_output.setText(mesure.toString());
-                    chlore_date_output.setText(date.toString());
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                Toast toast = Toast.makeText(getContext(), toast_info_ph, Toast.LENGTH_LONG);
+                toast.show();
             }
         });
+
+        // Toast of information for the chlore
+        ImageView info_chlore = (ImageView) getView().findViewById(R.id.icon_info_chlore);
+        info_chlore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getContext(), toast_info_chlore, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        // Toast of information for the temperature
+        ImageView info_temp = (ImageView) getView().findViewById(R.id.icon_info_temp);
+        info_temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getContext(), toast_info_temp, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+
+    public void getMesure() {
+
+        final TextView ph_output =(TextView) getView().findViewById(R.id.output_ph);
+        final TextView chlore_output =(TextView) getView().findViewById(R.id.output_chlore);
+        final TextView temp_output = (TextView) getView().findViewById(R.id.output_temp);
+        final TextView date_output = (TextView) getView().findViewById(R.id.mesure_date);
+
+        Test_connectivity task_ph = new Test_connectivity();
+        Test_connectivity task_chlore = new Test_connectivity();
+        Test_connectivity task_temp = new Test_connectivity();
+
+        // Relative to pH and date
+        task_ph.execute("http://loicberthelot.freeboxos.fr/device/pH/");
+        try {
+            //get when the request is finished
+            String response = task_ph.get();
+            Log.i("Content",response);
+
+            JSONArray json = new JSONArray(response);
+            String ph_mesure = json.getJSONObject(0).getString("mesure");
+            String date = json.getJSONObject(0).getString("time_of_mesure");
+
+            ph_output.setText(ph_mesure.toString());
+            date_output.setText("relevé fait le : " + date.toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Relative to Chlore
+        task_chlore.execute("http://loicberthelot.freeboxos.fr/device/Chlore/");
+        try {
+            //get when the request is finished
+            String response = task_chlore.get();
+            Log.i("Content",response);
+
+            JSONArray json = new JSONArray(response);
+            String chlore_mesure = json.getJSONObject(0).getString("mesure");
+            //String date = json.getJSONObject(0).getString("time_of_mesure");
+
+            chlore_output.setText(chlore_mesure.toString());
+            //date_output.setText("relevé fait le : " + date.toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Relative to temperature
+        task_temp.execute("http://loicberthelot.freeboxos.fr/device/Temperature/");
+        try {
+            //get when the request is finished
+            String response = task_temp.get();
+            Log.i("Content",response);
+
+            JSONArray json = new JSONArray(response);
+            String temp_mesure = json.getJSONObject(0).getString("mesure");
+
+            temp_output.setText(temp_mesure.toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
